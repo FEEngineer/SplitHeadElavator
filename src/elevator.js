aefa53339c8362d7f24566c7a24dfbf58a831adb
qiuying <3154194978@qq.com>
2018-05-06 11:36:04 +0800

    fill in basic information of ROute

--- src/elevator.js
+++ src/elevator.js
@@ -1,4 +1,4 @@
-class Elavator {
+class Elevator {
     constructor() {
 
     }
--- src/router.js
+++ src/router.js
@@ -1,5 +1,30 @@
-class Router {
-    constructor() {
+/**
+ * used for routing of different policy
+ */
 
+class RouteBuilder {
+    constructor() { }
+
+    buildRouter(level, policy) {
+        switch (policy) {
+            case 'ELEVATOR_REPORT': {
+                break;
+            }
+            
+            case 'SCHEDULER_TRANSFER': {
+                break;
+            }
+
+            case 'CALL_ELEVATOR': {
+                break;
+            }
+        }
+    }
+}
+
+class Router {
+    constructor(level, policy) {
+        this.level = level;
+        this.policy = policy;
     }
 }
